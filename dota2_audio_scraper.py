45b61ba2fb39e32e899f169b7b83885681298f91
GTWFam <ppham@wpi.edu>
2020-08-19 15:40:18 -0400

    Retrieved all pages with responses

--- dota2_audio_scraper.py
+++ dota2_audio_scraper.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup as bs
 DOMAIN = 'https://dota2.gamepedia.com'
 URL = 'https://dota2.gamepedia.com/Category:Responses'
 FILETYPE = ".mp3"
+pages = []
 
 def get_soup(url) :
 	return bs(requests.get(url).text, 'html.parser')
@@ -11,4 +12,7 @@ def get_soup(url) :
 groups = get_soup(URL).findAll('div', {"class", "mw-category-group"})
 
 for code in groups:
-	print(code.h3.text)
+	for link in code.findAll('a'):
+		pages.append(link.get('href'))
+
+print(pages)
