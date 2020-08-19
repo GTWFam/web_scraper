52fd58d27b661950d5e985314147b4e99607d6b8
GTWFam <ppham@wpi.edu>
2020-08-19 19:59:44 -0400

    Creating folders for the files

--- dota2_audio_scraper.py
+++ dota2_audio_scraper.py
@@ -1,12 +1,14 @@
 import requests
+import os
 from bs4 import BeautifulSoup as bs
 
 DOMAIN = 'https://dota2.gamepedia.com'
 URL = 'https://dota2.gamepedia.com/Category:Responses'
 FILETYPE = ".mp3"
+PARENT_DIR = "/Users/hoangphanpham/Documents/Projects/Application/dota_audio_files"
 
 # Function to get the html of a page using the url
-def get_soup(url) :
+def get_soup(url):
 	return bs(requests.get(url).text, 'html.parser')
 
 # Function to get the file links and file names
@@ -30,7 +32,13 @@ def get_audio(url):
 
 	# print(len(file_links))
 	# print(len(file_names))
-	
+
+# Renaming the pages to create Directories
+def rename(page):
+	page = page.replace("/", "")
+	page = page.replace("Responses", "")
+	page = page.replace("%27", "'")
+	return page
 
 groups = get_soup(URL).findAll('div', {"class", "mw-category-group"})
 
@@ -39,28 +47,37 @@ for code in groups:
 	for link in code.findAll('a'):
 		pages.append(link.get('href'))
 
-# for aPage in pages:
-# 	file_links = []
-# 	file_names = []
-# 	get_audio(DOMAIN + aPage)
+for aPage in pages:
+	file_links = []
+	file_names = []
 
-# 	i = 0
-# 	while i < len(file_links):
-# 		with open("\Abadon\" + file_names[i] + FILETYPE, 'wb') as file:
-# 		response = requests.get(DOMAIN + file_links[i])
-# 		file.write(response.content)
-# 		i += 1
+	get_audio(DOMAIN + aPage)
 
+	directory = rename(aPage)
+	path = os.path.join(PARENT_DIR, directory)
+	os.mkdir(path)
 
+	i = 0
+	while i < len(file_links):
+		with open(os.path.join(path, file_names[i]) + FILETYPE, 'wb') as file:
+			response = requests.get(file_links[i])
+			file.write(response.content)
+			i += 1
 
-print(pages[0])
-file_links = []
-file_names = []
-get_audio(DOMAIN + pages[0])
+# aPage = rename(pages[5])
 
-with open(file_names[0] + FILETYPE, "wb") as file:
-	response = requests.get(file_links[0])
-	file.write(response.content)
+# print(aPage)
+
+# path = os.path.join(parent_dir, aPage)
+# os.mkdir(path)
+
+# file_links = []
+# file_names = []
+# get_audio(DOMAIN + pages[0])
+
+# with open(os.path.join(path, file_names[0]) + FILETYPE, "wb") as file:
+# 	response = requests.get(file_links[0])
+# 	file.write(response.content)
 
 
 
