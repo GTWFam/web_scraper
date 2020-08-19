5388d1e732366cc509c663d58e06c5ff6c8b472a
GTWFam <ppham@wpi.edu>
2020-08-19 17:52:50 -0400

    Created a loop to download all files

--- dota2_audio_scraper.py
+++ dota2_audio_scraper.py
@@ -5,22 +5,13 @@ DOMAIN = 'https://dota2.gamepedia.com'
 URL = 'https://dota2.gamepedia.com/Category:Responses'
 FILETYPE = ".mp3"
 
-
+# Function to get the html of a page using the url
 def get_soup(url) :
 	return bs(requests.get(url).text, 'html.parser')
 
-groups = get_soup(URL).findAll('div', {"class", "mw-category-group"})
-
-pages = []
-for code in groups:
-	for link in code.findAll('a'):
-		pages.append(link.get('href'))
-
+# Function to get the file links and file names
 def get_audio(url):
-
-	file_links = []
-	file_names = []
-
+	
 	content_location = get_soup(url).find('div', {"class", "mw-parser-output"})
 	
 	audio_links = content_location.findAll('a')
@@ -37,10 +28,41 @@ def get_audio(url):
 				aText = aName.text.replace("Link▶️", "")
 				file_names.append(aText)
 
-	print(len(file_links))
-	print(len(file_names))
+	# print(len(file_links))
+	# print(len(file_names))
 	
-print(pages[23])
-get_audio(DOMAIN + pages[23])
+
+groups = get_soup(URL).findAll('div', {"class", "mw-category-group"})
+
+pages = []
+for code in groups:
+	for link in code.findAll('a'):
+		pages.append(link.get('href'))
+
+# for aPage in pages:
+# 	file_links = []
+# 	file_names = []
+# 	get_audio(DOMAIN + aPage)
+
+# 	i = 0
+# 	while i < len(file_links):
+# 		with open("\Abadon\" + file_names[i] + FILETYPE, 'wb') as file:
+# 		response = requests.get(DOMAIN + file_links[i])
+# 		file.write(response.content)
+# 		i += 1
+
+
+
+print(pages[0])
+file_links = []
+file_names = []
+get_audio(DOMAIN + pages[0])
+
+with open(file_names[0] + FILETYPE, "wb") as file:
+	response = requests.get(file_links[0])
+	file.write(response.content)
+
+
+
 
 
